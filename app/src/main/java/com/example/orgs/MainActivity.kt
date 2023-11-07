d1fa0bed55ab563bdf14efd551398dba9c2a9f1b
djefani <djefani.eveeleen@gmail.com>
2023-11-07 14:02:09 -0300

    adicionando Layout, trabalhando com views

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,4 +1,16 @@
 package com.example.orgs
 
-class MainActivity {
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        //view.setText("Amamos Gatinhos")
+        setContentView(R.layout.activity_main)
+        //Toast.makeText(this,"Bem vindo(a) ao Orgs!", Toast.LENGTH_SHORT).show()
+    }
+
 }
